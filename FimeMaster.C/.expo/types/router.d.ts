fb852e2a8590848655e5ee1f863f9157dc86283f
jorgeein <77907542+jorgeein@users.noreply.github.com>
2024-10-06 16:00:26 -0600

    upd
    upd

--- FimeMaster.C/.expo/types/router.d.ts
+++ FimeMaster.C/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/(tabs)/regiser` | `/..\components\navigation\TabBarIcon` | `/_sitemap` | `/explore` | `/regiser`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/(tabs)/firstscreen` | `/(tabs)/register` | `/(tabs)\TerminarQuiz` | `/(tabs)\_layout` | `/(tabs)\crearQuiz` | `/(tabs)\login` | `/(tabs)\registro` | `/(tabs)\resolverBien` | `/_sitemap` | `/explore` | `/firstscreen` | `/quiz` | `/register`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
