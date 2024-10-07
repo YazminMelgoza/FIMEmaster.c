7c02b8ca50ebed7f0a0f3d9a18e1f431a02a6127
jorgeein <77907542+jorgeein@users.noreply.github.com>
2024-10-07 00:28:36 -0600

    file changes routes
    file route changes

--- FimeMaster.C/.expo/types/router.d.ts
+++ FimeMaster.C/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/TerminarQuiz` | `/_sitemap` | `/confirmation` | `/confirmscan` | `/crearQuiz` | `/historialquiz` | `/home` | `/login` | `/quiz` | `/registro` | `/resolverBien` | `/scan` | `/splashscreen` | `/verification`;
+      StaticRoutes: `/` | `/_sitemap` | `/home` | `/inicio/qr/confirmscan` | `/inicio/qr/scan` | `/inicio/quiz/TerminarQuiz` | `/inicio/quiz/crearQuiz` | `/inicio/quiz/quiz` | `/inicio/quiz/resolverBien` | `/inicio/user/historialquiz` | `/login/login` | `/register/confirmation` | `/register/registro` | `/register/verification`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
