2bc3be0a480811cd975cd991c1d648737e4288f5
MarioMedePrsc <rioma.mercury@gmail.com>
2024-10-12 00:34:03 -0600

    Nueva estructura añadida

---
+++ app/(tabs)/Stadistics.tsx
@@ -0,0 +1,13 @@
+import { Alert, StyleSheet, View, AppState, Pressable, Text } from 'react-native'
+
+export default function About(){
+    return(
+        <View>
+            <Text>AAAAAAAAAAA</Text>
+            <Text>AAAAAAAAAAA</Text>
+            <Text>Stadistics</Text>
+            <Text>AAAAAAAAAAA</Text>
+            <Text>AAAAAAAAAAA</Text>
+        </View>
+    )    
+}
