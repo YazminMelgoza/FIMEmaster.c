87d059f4eea7073985ba462073e3ec5a7e7cdf6a
MarioMedePrsc <rioma.mercury@gmail.com>
2024-10-13 23:23:57 -0600

    changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,16 @@
 import { Text, View } from 'react-native'
+import { Link, Stack } from "expo-router"
 export default function Index()
 {
     return <View> 
         <Text>Home</Text>
+        <Text>Home</Text>
+        <Text>Home</Text>
+        <Text>Home</Text>
+        <Text>Home</Text>
+        
+        <Link href="/signUp/test" >
+            <Text > Regístrate</Text>
+            </Link>
     </View>;
 }
