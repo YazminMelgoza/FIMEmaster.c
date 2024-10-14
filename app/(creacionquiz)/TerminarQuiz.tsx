c71edf9d13700092cf60e0f64125f03c1475c26b
MarioMedePrsc <rioma.mercury@gmail.com>
2024-10-14 00:54:26 -0600

    Se han añadido todas las pantallas creadas actualmente

---
+++ app/(creacionquiz)/TerminarQuiz.tsx
@@ -0,0 +1,96 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+export default function TerminarQuizScreen() {
+    return (
+        <View style={styles.containerMain}>
+            <View style={styles.containerTop}>
+                <StatusBar style="dark" backgroundColor='#ffffff' />
+                <View style={styles.row}>
+                    <TouchableOpacity>
+                        <Image
+                            source={require('../../assets/images/back-icon.png')}
+                            style={styles.buttonImage}
+                        />
+                    </TouchableOpacity>
+                    <Text style={styles.textTop}>Quiz Finalizado</Text>
+                </View>
+            </View>
+            <View style={styles.column}>  
+                    <Image
+                        source={require('../../assets/images/succesful-img.png')}
+                        style={styles.imageMain}
+                    />
+                    <Text style={styles.textMain}>Haz finalizado el ejercicio</Text>
+                    <Text style={styles.textPoints}>+10 puntos</Text>
+                    <TouchableOpacity style={styles.buttonBack}>
+                        <Text style={styles.textButton}>Volver al inicio</Text>
+                    </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    containerTop: {
+        height: 150,
+        backgroundColor: '#178F49',
+        justifyContent: 'center',
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    column: {
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    textTop: {
+        marginTop: 20,
+        fontSize: 24,
+        color: '#ffffff',
+    },
+    containerMain: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    imageContainer: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    imageMain: {
+        width: 194,
+        height: 190,
+    },
+    buttonImage: {
+        width: 13,
+        height: 24,
+        marginLeft: 41,
+        marginTop: 20,
+        marginRight: 50,
+    },
+    buttonBack: {
+        backgroundColor: '#179659',
+        borderRadius: 15,
+        paddingVertical: 10,
+        paddingHorizontal: 100,
+        marginTop: 110,
+    },
+    textButton: {
+        color: '#fff',
+        fontSize: 13,
+    },
+    textMain: {
+        color: '#178F49',
+        fontSize: 32,
+        fontWeight: 'bold',
+        marginVertical: 10,
+        textAlign: 'center',
+    },
+    textPoints: {
+        color: '#3BA76B',
+        fontSize: 24,
+        marginVertical: 20,
+    },
+});
