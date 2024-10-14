70cd689c4877d2b05718da692d903d4ae1d5230d
Derick Veloz <derick.velozarand@gmail.com>
2024-10-14 17:48:59 -0600

    Se agrego funcionalidad a los botones y se redondeo el contenedor principal

--- app/(tabs)/TerminarQuiz.tsx
+++ app/(tabs)/TerminarQuiz.tsx
@@ -1,31 +1,48 @@
 import { View, Text, StyleSheet, Image, TouchableOpacity, ImageBackground } from 'react-native';
+import { useRouter } from 'expo-router';
+import React, { useState } from 'react';
 
 export default function TerminarQuizScreen() {
+
+    const router = useRouter();
+
     return (
-        <View style={styles.containerMain}>
-            <ImageBackground
-                source={require('../../assets/images/relieve.png')}
-                style={styles.containerTop}
-            >
-                <View style={styles.row}>
-                    <TouchableOpacity>
-                        <Image
-                            source={require('../../assets/images/back-icon.png')}
-                            style={styles.buttonImage}
-                        />
-                    </TouchableOpacity>
-                    <Text style={styles.textTop}>Quiz Finalizado</Text>
-                </View>
-            </ImageBackground>
-            <View style={styles.column}>  
+        /* HEADER */
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image
+                    source={require('../../assets/images/relieve.png')}
+                    style={styles.headerBackgroundImage}
+                />
+                <View style={styles.headerContent}>
+                    <View style={styles.headerTextContainer}>
+                        <View style={styles.backContainer}>
+                            <TouchableOpacity
+                                onPress={() => router.back()}>
+                                <Image
+                                    source={require('../../assets/images/back-icon.png')}
+                                    style={styles.backImage}/>
+                            </TouchableOpacity>
+                        </View>
+                        <Text style={styles.headerText}>Quiz finalizado</Text>
+                    </View>
+                </View>     
+            </View>
+
+            {/* MAIN */}
+
+            <View style={styles.whiteBackgroundContainer}>
                 <Image
                     source={require('../../assets/images/succesful-img.png')}
                     style={styles.imageMain}
-                />
+                    />
                 <Text style={styles.textMain}>Haz finalizado el ejercicio</Text>
                 <Text style={styles.textPoints}>+10 puntos</Text>
-                <TouchableOpacity style={styles.buttonBack}>
-                    <Text style={styles.textButton}>Volver al inicio</Text>
+                <TouchableOpacity 
+                    style={styles.buttonBack}
+                    onPress={() => router.back()}
+                >
+                <Text style={styles.textButton}>Volver al inicio</Text>
                 </TouchableOpacity>
             </View>
         </View>
@@ -33,26 +50,67 @@ export default function TerminarQuizScreen() {
 }
 
 const styles = StyleSheet.create({
-    containerTop: {
-        height: 150,
-        justifyContent: 'center',
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
     },
-    row: {
+    header: {
+        flexDirection: 'column', 
+        padding: 20,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        paddingBottom: 40,
+        marginBottom: 0,
+        position: 'relative', 
+    },
+    headerBackgroundImage: {
+        width: 1000,
+        height: 250,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        zIndex: 0,
+    },
+    headerContent: {
+        flexDirection: 'row',
+        marginTop: 40, 
+    },
+    headerTextContainer: {
         flexDirection: 'row',
         alignItems: 'center',
+        marginLeft: 0, 
     },
-    column: {
-        flexDirection: 'column',
-        alignItems: 'center',
+    backContainer: {
+        flexDirection: 'row',
     },
-    textTop: {
+    backImage: {
         marginTop: 20,
+        marginLeft: 20,
+        width: 13,
+        height: 24,
+    },
+    headerText: {
+        marginTop: 20,
+        marginLeft: 51,
         fontSize: 24,
         color: '#ffffff',
     },
-    containerMain: {
-        flex: 1,
+    whiteBackgroundContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexGrow: 1,
         backgroundColor: '#fff',
+        borderTopLeftRadius: 50,
+        borderTopRightRadius: 50,
+        padding: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        elevation: 3,
+        zIndex: 1,
     },
     imageContainer: {
         flex: 1,
