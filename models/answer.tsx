2e82f3bf33e3b383912d829ff1a9303c9162df88
YazminMelgoza <yazmin.melgozaz@uanl.edu.mx>
2024-10-30 21:26:23 -0600

    se sustituyen los modelos por los generados automaticamente por supabase y se corrigen los servicios para implementarlos correctamente
    - El servicio de quiz fue renombrado a exercise para coincidir con la estructura de la base de datos
    - el campo id de users cambio a ser userId

--- models/answer.tsx
+++
@@ -1,7 +0,0 @@
-// answerModel.ts
-export type Answer = {
-    answerid: number;       // ID de la respuesta
-    questionid: number;     // ID de la pregunta asociada
-    answer: string;         // Texto de la respuesta
-    iscorrect: boolean;     // Indica si la respuesta es correcta
-}
--- services/user.tsx
+++ services/user.tsx
@@ -1,32 +1,35 @@
-import { supabase } from '../lib/supabase';
-import { User } from '../models/user';
-import { PostgrestError } from '@supabase/supabase-js';
-
+import { supabase } from "../lib/supabase";
+import { PostgrestError } from "@supabase/supabase-js";
+import { Tables } from "database.types";
 export class UserService {
-
   // Función para obtener un usuario por ID
-  static async getUserById(id: string): Promise<{ user: User | null; error: PostgrestError | null }> {
+  static async getUserProfileById(id: string): Promise<{
+    user: Tables<"users"> | null;
+    error: PostgrestError | null;
+  }> {
     const { data, error } = await supabase
-      .from('profiles') // Cambia 'profiles' al nombre de tu tabla si es diferente
-      .select('*')
-      .eq('id', id)
+      .from("users")
+      .select("*")
+      .eq("userId", id)
       .single(); // Solo esperamos un registro
 
     if (error) {
-      console.error('Error al obtener el usuario:', error);
+      console.error("Error al obtener el usuario:", error);
       return { user: null, error };
     }
 
-    console.log('Usuario obtenido:', data);
-    return { user: data as User, error: null };
+    console.log("Usuario obtenido:", data);
+    return { user: data, error: null };
   }
   // Función para actualizar el perfil de un usuario
-  static async updateUserById(user: User): Promise<PostgrestError | null> {
+  static async updateUserProfileById(
+    user: Tables<"users">
+  ): Promise<PostgrestError | null> {
     try {
       const { error } = await supabase
-        .from('profiles')
+        .from("users")
         .update({
-          id: user.id,
+          userId: user.userId,
           username: user.username,
           website: user.website,
           avatar_url: user.avatar_url,
@@ -35,17 +38,15 @@ export class UserService {
           middlename: user.middlename,
           updated_at: new Date().toISOString(),
         })
-        .eq('id', user.id);
+        .eq("userId", user.userId);
 
-      if (error) {
-        return error; // Devuelve el error de Supabase si hay un error
-      }
-      console.log('Perfil actualizado:', user);
+      if (error) return error; // Devuelve el error de Supabase si hay un error
+      console.log("Perfil actualizado:", user);
       return null; // Sin error
     } catch (err) {
-      console.error('Error al actualizar el perfil:', err);
+      console.error("Error al actualizar el perfil:", err);
       // Devuelve null si se produce un error inesperado
-      return null; 
+      return null;
     }
   }
 }
